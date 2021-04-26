8b24a01da2b35228ae74290512b98073f6232620
Priscyla <primarcal_@hotmail.com>
2021-04-26 00:33:54 -0300

    pasta do banco de dados
    adicionado a pasta onde contém os arquivos do banco de dados.

---
+++ bd/tabelas.sql
@@ -0,0 +1,21 @@
+create table PAISES(
+    codPais integer not null primary key,
+    pais varchar(50) not null,
+    ddi varchar(4),
+    sigla varchar(3) not null,
+    moeda varchar(3),
+    dataCad timestamp,
+    ultAlt timestamp,
+    codUsuAlt integer
+);
+
+create table ESTADOS(
+    codEstado integer not null primary key,
+    estado varchar(50) not null,
+    uf varchar(2) not null,
+    codPais integer not null,
+    dataCad timestamp,
+    ultAlt timestamp,
+    codUsuAlt integer,
+	constraint fk_Estado_Pais foreign key (codPais) references PAISES (codPais)
+);
